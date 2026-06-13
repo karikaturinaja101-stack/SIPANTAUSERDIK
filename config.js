9d7a583c547b2c48c2238ee041bc5c68d4f49d89
karikaturinaja101-stack <karikaturinaja101@gmail.com>
2026-06-14 07:26:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Isi dua nilai di bawah ini dari Project Settings > API di Supabase.
 
 window.SIPANTAU_SUPABASE = {
-  url: "ISI_SUPABASE_URL_ANDA",
-  anonKey: "ISI_SUPABASE_ANON_KEY_ANDA"
+  url: "https://eycwectpwbmvmisxbxkl.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5Y3dlY3Rwd2Jtdm1pc3hieGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNjE0NTgsImV4cCI6MjA5NjkzNzQ1OH0.N0b4xPJPFemHAegksAGLPiYzW_V0IwZY5VH3PRLTQMs"
 };
